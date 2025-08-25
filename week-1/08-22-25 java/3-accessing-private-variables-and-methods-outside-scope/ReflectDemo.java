96068d1f0ece0cf85bf6ba95ca8da0c58ef76471
Darshan V A <darshan.va@i-exceed.com>
2025-08-25 16:08:58 +0530

    22-08-25 assignment submitted

--- week-1/08-22-25 java/3-accessing-private-variables-and-methods-outside-scope/ReflectDemo.java
+++ week-1/08-22-25 java/3-accessing-private-variables-and-methods-outside-scope/ReflectDemo.java
@@ -15,11 +15,11 @@ class ReflectDemo{
 
 	public static void main(String args[]) throws Exception{
 
-		Class c = Class.forName("PrivateData");
-		Object obj = c.newInstance();
-		Method method = c.getDeclaredMethod("displayAccNo",String.class);
-		method.setAccessible(true);
-		method.invoke(obj,"test");
+		Class c = Class.forName("PrivateData");					//Creating an instance of Class
+		Object obj = c.newInstance();						//Creating an instance of Object class
+		Method method = c.getDeclaredMethod("displayAccNo",String.class);	//Creating an instance of Method class and specifing variables
+		method.setAccessible(true);						//setting the method as accessible
+		method.invoke(obj,"test");						//calling the method by passing parameters
 
 	}
 
